5d7063f96c45e0a11e8f702b9488473cbff237aa
Michael Bykov <m.bykov@gmail.com>
2015-07-19 23:30:28 +0300

    iast start

--- salita.js
+++ salita.js
@@ -7,6 +7,8 @@ try {
     var each = require('each');
 }
 
+var c = require('./lib/constants');
+
 module.exports = salita();
 
 function salita() {
@@ -60,6 +62,39 @@ salita.prototype.sa2slp = function(str) {
     });
     return slp.join('');
 }
+
+salita.prototype.sa2iast = function(str) {
+    // неоднозначные - с придыханием, сначала их, потом все подряд
+    // если гласная-лига, то - если a - одно, иначе - другое
+    str = clean(str);
+    var cons = invert(c.consIAST);
+    var  ligas = invert(c.ligaIAST);
+    var  vowels = invert(c.vowelIAST);
+    // var  signs = invert(signs);
+    var arr = str.split('');
+    var iast = [];
+    log('==>', arr)
+    arr.forEach(function(sym, idx) {
+        var prev = arr[idx-1];
+        var next = arr[idx+1];
+        // log('SYM', prev, sym, next);
+        if (sym in cons) {
+            iast.push(cons[sym]);
+            if (next in cons) {
+                iast.push('a');
+            } else if (next in ligas) {
+                iast.push(ligas[next]);
+            } else if (!next) {
+                iast.push('a');
+            }
+        }
+    });
+    log('IAST', iast);
+    // return 'satyānṛta';
+    // return 'padaccheda';
+    return iast.join('');
+}
+
 
 var signs = {
     "/": "́",
@@ -138,83 +173,6 @@ var consonants = {
     "": "",
 };
 
-// Harvard-Kioto (based on)
-
-var VowelsHK = {
-    "a": "अ",
-    "aa": "आ",
-    "A": "आ",
-    "i": "इ",
-    "ii": "ई",
-    "I": "ई",
-    "u": "उ",
-    "uu": "ऊ",
-    "U": "ऊ",
-    "e": "ए",
-    "ai": "ऐ",
-    "o": "ओ",
-    "au": "औ",
-    "R": "ऋ",
-    "RR": "ॠ",
-    "lR": "ऌ",
-    "lRR": "ॡ"
-}
-
-var vowelsHK = {
-    "a": "|", // '|' is for 'non virama' sign only
-    "aa": "ा",
-    "A": "ा",
-    "i": "ि",
-    "ii": "ी",
-    "I": "ी",
-    "u": "ु",
-    "uu": "ू",
-    "U": "ू",
-    "e": "े",
-    "ai": "ै",
-    "o": "ो",
-    "au": "ौ",
-    "R": "ृ",
-    "RR": "ॄ",
-    "lRR": "ॢ",
-}
-
-var consonantsHK = {
-    "k": "क",
-    "kh": "ख",
-    "g": "ग",
-    "gh": "घ",
-    "G": "ङ",
-    "c": "च",
-    "ch": "छ",
-    "j": "ज",
-    "jh": "झ",
-    "J": "ञ",
-    "T": "ट",
-    "Th": "ठ",
-    "D": "ड",
-    "Dh": "ढ",
-    "N": "ण",
-    "t": "त",
-    "th": "थ",
-    "d": "द",
-    "dh": "ध",
-    "n": "न",
-    "p": "प",
-    "ph": "फ",
-    "b": "ब",
-    "bh": "भ",
-    "m": "म",
-    "y": "य",
-    "r": "र",
-    "l": "ल",
-    "v": "व",
-    "z": "श",
-    "S": "ष",
-    "s": "स",
-    "h": "ह",
-    "": "",
-};
 
 salita.prototype.hk2sa = function(str) {
     str = clean(str);
@@ -286,8 +244,6 @@ var Const = {
 }
 
 
-function log() { console.log.apply(console, arguments) }
-
 function invert(obj) {
     var new_obj = {};
     for (var prop in obj) {
@@ -297,3 +253,5 @@ function invert(obj) {
     }
     return new_obj;
 };
+
+function log() { console.log.apply(console, arguments) }
--- test/node/salita.js
+++ test/node/salita.js
@@ -58,6 +58,64 @@ describe('salita devanagari converter', function() {
         //     salita.sa2slp('').should.equal('');
         // });
     });
+
+    describe('iast', function() {
+        it('convert satyAnfta', function() {
+            salita.sa2iast('सत्यानृत').should.equal('satyānṛta');
+        });
+        it('convert padacCeda', function() {
+            salita.sa2iast('पदच्छेद').should.equal('padaccheda');
+        });
+
+        it('convert satya', function() {
+            salita.slp2sa('satya').should.equal('सत्य');
+            salita.sa2slp('सत्य').should.equal('satya');
+        });
+        it('convert anfta', function() {
+            salita.slp2sa('anfta').should.equal('अनृत');
+            salita.sa2slp('अनृत').should.equal('anfta');
+        });
+        it('convert rAma', function() { // राम (rāma) + ऋद्धि (ṛddhi)
+            salita.slp2sa('rAma').should.equal('राम');
+        });
+        it('convert fdDi', function() {
+            salita.slp2sa('fdDi').should.equal('ऋद्धि');
+            salita.sa2slp('ऋद्धि').should.equal('fdDi');
+        });
+        it('convert Uru', function() { // ऊरु Uru
+            salita.slp2sa('Uru').should.equal('ऊरु'); //
+            salita.sa2slp('ऊरु').should.equal('Uru');
+        });
+        it('convert kena', function() {
+            salita.slp2sa('kena').should.equal('केन');
+            salita.sa2slp('केन').should.equal('kena');
+        });
+        it('convert upanizat', function() {
+            salita.slp2sa('upanizat').should.equal('उपनिषत्');
+            salita.sa2slp('उपनिषत्').should.equal('upanizat');
+        });
+        it('convert kxpta', function() {
+            salita.slp2sa('kxpta').should.equal('कॢप्त');
+            salita.sa2slp('कॢप्त').should.equal('kxpta');
+        });
+        it('convert sraMsin', function() {
+            salita.slp2sa('sraMsin').should.equal('स्रंसिन्');
+            salita.sa2slp('स्रंसिन्').should.equal('sraMsin');
+        });
+        it('convert saMkA', function() {
+            salita.slp2sa('saMkA').should.equal('संका');
+            salita.sa2slp('संका').should.equal('saMkA');
+        });
+        it('convert apasalEH', function() {
+            salita.slp2sa('apasalEH').should.equal('अपसलैः');
+            salita.sa2slp('अपसलैः').should.equal('apasalEH');
+        });
+        // it('convert ', function() {
+        //     salita.slp2sa('').should.equal('');
+        //     salita.sa2slp('').should.equal('');
+        // });
+    });
+
 });
 
 
